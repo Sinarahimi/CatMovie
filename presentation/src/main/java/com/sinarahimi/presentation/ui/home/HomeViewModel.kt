7e200950e095ca7e372aaa51fd57abed747a82bb
develop.rahimi95 <develop.rahimi95@gmail.com>
2020-02-26 17:48:57 +0330

    Create structure for the project

---
+++ presentation/src/main/java/com/sinarahimi/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,14 @@
+package com.sinarahimi.presentation.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+//
+// Created by Sina Rahimi on 2/26/20.
+//
+
+class HomeViewModel :ViewModel(){
+
+
+    val getTradingLiveData:LiveData<>
+}
